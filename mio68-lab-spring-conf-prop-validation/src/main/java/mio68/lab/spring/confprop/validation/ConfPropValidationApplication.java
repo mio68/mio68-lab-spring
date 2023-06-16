bafc5ecd9c691f89d600d1c66aefb5ad45255678
Igor Markivskiy <mio6823@gmail.com>
2023-06-16 09:36:19 +0300

    configuration properties validation for list elements

--- mio68-lab-spring-conf-prop-validation/src/main/java/mio68/lab/spring/confprop/validation/ConfPropValidationApplication.java
+++ mio68-lab-spring-conf-prop-validation/src/main/java/mio68/lab/spring/confprop/validation/ConfPropValidationApplication.java
@@ -1,8 +1,14 @@
 package mio68.lab.spring.confprop.validation;
 
+import lombok.RequiredArgsConstructor;
+import mio68.lab.spring.confprop.validation.properties.AppProperties;
+import mio68.lab.spring.confprop.validation.properties.OtherProperties;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -12,6 +18,19 @@ public class ConfPropValidationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfPropValidationApplication.class, args);
+    }
+
+    @Component
+    @RequiredArgsConstructor
+    public static class ConfigurationPrinter implements ApplicationRunner {
+        private final OtherProperties otherProperties;
+        private final AppProperties appProperties;
+
+        @Override
+        public void run(ApplicationArguments args) {
+            System.out.println(appProperties);
+            System.out.println(otherProperties);
+        }
     }
 
 }
--- mio68-lab-spring-conf-prop-validation/src/main/java/mio68/lab/spring/confprop/validation/properties/OtherProperties.java
+++ mio68-lab-spring-conf-prop-validation/src/main/java/mio68/lab/spring/confprop/validation/properties/OtherProperties.java
@@ -5,6 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
 
 @ConfigurationProperties("mio68.lab")
 @Data
@@ -13,5 +16,10 @@ public class OtherProperties {
 
     @NotBlank
     private String module;
+
+    @NotEmpty
+    private List<String> cns = new ArrayList<>();
+
+    private List<@NotBlank String> aliases = new ArrayList<>();
 
 }
